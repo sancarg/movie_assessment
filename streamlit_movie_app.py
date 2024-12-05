8362049b408bc33ccb833fadc8b8eda0701c97dc
sancarg <94074200+sancarg@users.noreply.github.com>
2024-12-05 10:37:31 +0300

    Update streamlit_movie_app.py

--- streamlit_movie_app.py
+++ streamlit_movie_app.py
@@ -3,6 +3,18 @@ import numpy as np
 import pandas as pd
 import altair as alt
 
-st.title('📽 App Movie Assessment App')
+# Set the page configuration 
+st.set_page_config(page_title="Movie Insights Analyzer", page_icon="📽", layout="wide")
 
+st.header('Movie Assessment App')
 st.info('This is a machine learning app for TMDB 3000+ Movie Dataset!')
+
+st.markdown('**What can you do with this app?**')
+st.info('This application demonstrates the capabilities of Pandas for data manipulation, Altair for visualizing charts, and an interactive dataframe for user engagement.')
+st.markdown('**How to use the app?**')
+st.warning('To engage with the app, 1. Select genres of your interest in the drop-down selection box and then 2. Select the year duration from the slider widget. As a result, this should generate an updated editable DataFrame and line plot.')
+
+# Question header
+st.subheader('Which Movie Genre performs ($) best at the box office?')
+# Load data - Read CSV into a Pandas DataFrame
+df = pd.read_csv('
